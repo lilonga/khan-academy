28a75ded3ca7d4f0fd4e27708bab32fb626557a4
L <manguluka@gmx.com>
2019-05-06 19:24:35 -0700

    create test for fetchChallenge function

--- test/index.int.test.js
+++ test/index.int.test.js
@@ -7,3 +7,11 @@ describe("fetchScratchpad", function() {
 		expect(res.data.scratchpad.relativeUrl).toBe("/computer-programming/spin-off-of-project-whats-for-dinner/6259327758336000");
 	});
 });
+
+describe("fetchChallenge", function() {
+	it("should fetch correct data for https://www.khanacademy.org/computing/computer-programming/html-css/html-tags-continued/pc/challenge-jump-around", async () => {
+		const res = await KA.fetchChallenge({ id:"x712ae29530655756" });
+		expect(res.status).toBe(200);
+		expect(res.data.scratchpad.defaultUrlPath).toBe("/computing/computer-programming/html-css/html-tags-continued/pc/challenge-jump-around");
+	});
+});
